bd97bba0b4e0da2f633f17c4ddc1f49cb0fd20d9
MrWilllian <willian.dev10@gmail.com>
2020-02-11 16:53:20 -0300

    feat: add animations

---
+++ fab/src/components/FabButton.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableWithoutFeedback, Animated } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+export default class components extends Component {
+  animation = new Animated.Value(0);
+
+  toggleMenu = () => {
+    const toValue = this.open ? 0 : 1;
+
+    Animated.spring(this.animation, {
+      toValue,
+      friction: 6,
+    }).start();
+
+    this.open = !this.open;
+  }
+
+  render() {
+    const cameraStyle = {
+      transform: [
+        { scale: this.animation },
+        {
+          translateY: this.animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -80]
+          })
+        }
+      ]
+    }
+
+    const heartStyle = {
+      transform: [
+        { scale: this.animation },
+        {
+          translateY: this.animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: [0, -160]
+          })
+        }
+      ]
+    }
+
+    const rotation = {
+      transform: [
+        {
+          rotate: this.animation.interpolate({
+            inputRange: [0, 1],
+            outputRange: ["0deg", "45deg"]
+          })
+        }
+      ]
+    }
+
+    return (
+      <View style={[styles.container, this.props.style]}>
+
+        <TouchableWithoutFeedback>
+          <Animated.View style={[styles.button, styles.submenu, heartStyle]}>
+            <Icon name="heart" size={20} color="#FFF" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback>
+          <Animated.View style={[styles.button, styles.submenu, cameraStyle]}>
+            <Icon name="camera" size={20} color="#FFF" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+        <TouchableWithoutFeedback onPress={this.toggleMenu}>
+          <Animated.View style={[styles.button, styles.menu, rotation]}>
+            <Icon name="plus" size={24} color="#FFF" />
+          </Animated.View>
+        </TouchableWithoutFeedback>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    position: 'absolute',
+  },
+  button: {
+    position: 'absolute',
+    width: 60,
+    height: 60,
+    borderRadius: 60/2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 10,
+    shadowColor: '#00213B',
+    shadowOpacity: 0.3,
+    shadowOffset: {
+      height: 10,
+    }
+  },
+  menu: {
+    backgroundColor: '#00213B',
+  },
+  submenu: {
+    width: 48,
+    height: 48,
+    borderRadius: 48/2,
+    backgroundColor: '#00213B',
+  }
+});
